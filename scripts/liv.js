d2c4d09528b6b9994b03b8b651c023597aae2b4d
aturnage <aturnage@hotmail.com>
2019-08-21 12:05:02 -0500

    Added com[pany description, info, and skills to about page

--- scripts/liv.js
+++ scripts/liv.js
@@ -1,1 +1,6 @@
 document.querySelector("#tagline").innerHTML = "Liv Outside the Box";
+document.querySelector(".descriptionTitle").innerHTML = "Liv Outside the Box";
+document.querySelector(".descriptionTitle").style.textAlign = "center"
+document.querySelector(".descriptionOne").innerHTML = "A business architect company with a vision to contribute to building innovative, incredible businesses, and brands by creating new channels of thought and actions through business business developement, marketing, branding, and advertising services."
+document.querySelector(".descriptionTwo").innerHTML = "Since opening in 2014, Liv has grown from a business consultant service to a full-service business architect company supporting business and personal brands express their stories to the world."
+document.querySelector(".descriptionFinal").innerHTML = "You are great ... now let's LIV OUTSIDE THE BOX";
