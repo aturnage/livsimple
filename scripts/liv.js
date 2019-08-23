a90a4cb8c0d8687e24872993e2bab08d7e14591f
Andrew <aturnage@hotmail.com>
2019-08-22 21:29:13 -0500

    Updated styles to the about page

--- scripts/liv.js
+++ scripts/liv.js
@@ -1,6 +1,18 @@
-document.querySelector("#tagline").innerHTML = "Liv Outside the Box";
-document.querySelector(".descriptionTitle").innerHTML = "Liv Outside the Box";
-document.querySelector(".descriptionTitle").style.textAlign = "center"
+
+// document.querySelector("#tagline").innerHTML = "Liv Outside the Box";
+
+// Liv title
+// document.querySelector(".descriptionTitle").innerHTML = "Liv Outside the Box";
+document.querySelector(".descriptionTitle").style.cssText = "text-align: center;font-family: 'Cousine', monospace; color: rgba(36, 36, 37, 1); margin: 20px 0;";
+
+// Liv description One
 document.querySelector(".descriptionOne").innerHTML = "A business architect company with a vision to contribute to building innovative, incredible businesses, and brands by creating new channels of thought and actions through business business developement, marketing, branding, and advertising services."
+document.querySelector("div.descriptionOne").style.cssText = "text-align: center; color: rgba(36,36,37, 1); font-family: 'Cousine', monospace;margin: 20px 0;";
+
+// Liv description Two
 document.querySelector(".descriptionTwo").innerHTML = "Since opening in 2014, Liv has grown from a business consultant service to a full-service business architect company supporting business and personal brands express their stories to the world."
+document.querySelector("div.descriptionTwo").style.cssText = "text-align: center; color: rgba(36,36,37, 1); font-family: 'Cousine', monospace;margin: 20px 0;";
+
+// Liv Final quote
 document.querySelector(".descriptionFinal").innerHTML = "You are great ... now let's LIV OUTSIDE THE BOX";
+document.querySelector(".descriptionFinal").style.cssText = "text-align: center;font-family: 'Cousine', monospace; color: rgba(36, 36, 37, 1);margin: 20px 0;";
