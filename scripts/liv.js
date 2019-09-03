96ae978132a8fe2eb7e33558d98aebda63399374
aturnage <aturnage@hotmail.com>
2019-09-03 13:58:38 -0500

    Added additonal company description and title styling to the about page

--- scripts/liv.js
+++ scripts/liv.js
@@ -1,6 +1,7 @@
-document.querySelector("#tagline").innerHTML = "Liv Outside the Box";
-document.querySelector(".descriptionTitle").innerHTML = "Liv Outside the Box";
+// document.querySelector("#tagline").innerHTML = "Liv Outside the Box";
+document.querySelector(".titleOne").innerHTML = "Liv Outside the Box";
 document.querySelector(".descriptionTitle").style.textAlign = "center"
 document.querySelector(".descriptionOne").innerHTML = "A business architect company with a vision to contribute to building innovative, incredible businesses, and brands by creating new channels of thought and actions through business business developement, marketing, branding, and advertising services."
-document.querySelector(".descriptionTwo").innerHTML = "Since opening in 2014, Liv has grown from a business consultant service to a full-service business architect company supporting business and personal brands express their stories to the world."
+document.querySelector(".descriptionTwo").innerHTML = "Since opening in 2014, Liv has grown from a business consultant service to a full-service business architect company supporting business and personal brands express their stories to the world. We have collaborated with over 30 businesses across the United States."
 document.querySelector(".descriptionFinal").innerHTML = "You are great ... now let's LIV OUTSIDE THE BOX";
+document.querySelector(".whatWeDo").innerHTML = "What We Do";
