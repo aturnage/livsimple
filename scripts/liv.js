44f3caf78e56abd4a76d82f0129f0da6779e0626
aturnage <aturnage@hotmail.com>
2019-09-11 15:44:25 -0500

    Updating with puzzle main pic

--- scripts/liv.js
+++ scripts/liv.js
@@ -1,7 +1,16 @@
 // document.querySelector("#tagline").innerHTML = "Liv Outside the Box";
+function load_home() {
+     document.querySelector(".hop").innerHTML='<object type="text/html" data="header.html" ></object>';
+}
 document.querySelector(".titleOne").innerHTML = "Liv Outside the Box";
 document.querySelector(".descriptionTitle").style.textAlign = "center"
 document.querySelector(".descriptionOne").innerHTML = "A business architect company with a vision to contribute to building innovative, incredible businesses, and brands by creating new channels of thought and actions through business business developement, marketing, branding, and advertising services."
 document.querySelector(".descriptionTwo").innerHTML = "Since opening in 2014, Liv has grown from a business consultant service to a full-service business architect company supporting business and personal brands express their stories to the world. We have collaborated with over 30 businesses across the United States."
 document.querySelector(".descriptionFinal").innerHTML = "You are great ... now let's LIV OUTSIDE THE BOX";
 document.querySelector(".whatWeDo").innerHTML = "What We Do";
+
+function load_home() {
+     document.query("hop").innerHTML='<object type="text/html" data="header.html" ></object>';
+}
+
+load_home();
